4a700acf9da0a8b65c722a77bfaac3dfbe80550c
Vivek Murali <vivekmurali00@outlook.com>
2022-06-08 23:20:33 +0530

    Connection of MongoDb with Data point

---
+++ Airflow Implementation/Kitsu ETL/dags/push_data.py
@@ -0,0 +1,16 @@
+from ...Utils.mongodb import Database
+import json
+import logging
+
+
+
+class ETL:
+    def __init__(self):
+        Database.initialize()
+
+    def etlpipelineanime(self):
+        with open('anime_sources.json','r') as fp:
+            data = json.load(fp)
+            fp.close()
+        records = [x for j in data for x in j]
+        Database.insert_many('nimesources', records)
