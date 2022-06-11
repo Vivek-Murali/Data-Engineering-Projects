cbc0b833f40d8d882e1a52f17ec006f1488a9f3f
Vivek Murali <vivekmurali00@outlook.com>
2022-06-12 00:08:36 +0530

    Added Uitlity Tools for tar files

---
+++ Airflow Implementation/CORE ETL/Makedata.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import json
+import logging as logger
+from os import listdir
+from os.path import isfile, join
+import sys
+from  pathlib import Path 
+import tarfile
+import os
+import shutil
+
+
+logger.basicConfig(format='%(asctime)s-%(levelname)s-%(message)s', level=logger.INFO, datefmt="%d-%b-%y %H:%M:%S")
+
+def extract_json_data(filename:str):
+    onlyfiles = [f for f in listdir(filename) if isfile(join(filename, f))]
+    [make_json_data(os.path.join("Data",f)) for f in onlyfiles if f not in listdir(os.path.join("json","Data"))]
+    logger.info("Extracting Process Done")
+
+
+def make_json_data(filename:str):
+    try:
+        tfile = tarfile.open(filename,"r:xz")
+        tfile.extractall(path=os.path.join("json",filename))
+        tfile.close()
+    except:
+        pass
+    
+def push_data_into_one():
+    Destination:str = os.path.join("json","Final")
+    [shutil.move(os.path.join(path, name), os.path.join(Destination,name)) for path, subdirs, files in os.walk(os.path.join("json","Data")) for name in files if name.endswith('.json')]
+    logger.info("Files Movement Completed")
+
+
+if __name__ == '__main__':
+    #extract_json_data("Data")
+    push_data_into_one()
