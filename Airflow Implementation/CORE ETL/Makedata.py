fe8e0e1c0ff92aa2e6797ceefdc5b373932dfc9a
Vivek Murali <vivekmurali00@outlook.com>
2022-06-14 09:52:16 +0530

    Added Asyncio Functinality for extraction

--- Airflow Implementation/CORE ETL/Makedata.py
+++ Airflow Implementation/CORE ETL/Makedata.py
@@ -9,19 +9,24 @@ from  pathlib import Path
 import tarfile
 import os
 import shutil
-
+import random
+import aiofiles
+import asyncio
 
 logger.basicConfig(format='%(asctime)s-%(levelname)s-%(message)s', level=logger.INFO, datefmt="%d-%b-%y %H:%M:%S")
 
 def extract_json_data(filename:str):
     onlyfiles = [f for f in listdir(filename) if isfile(join(filename, f))]
-    [make_json_data(os.path.join("Data",f)) for f in onlyfiles if f not in listdir(os.path.join("json","Data"))]
+    logger.info("Number of Files to Process %s"%len(onlyfiles))
+    onlyfiles = random.choices(onlyfiles,k=30)
+    [make_json_data(os.path.join("Data",f),number) for number,f in enumerate(onlyfiles) if f not in listdir(os.path.join("json","Data"))]
     logger.info("Extracting Process Done")
 
 
-def make_json_data(filename:str):
+def make_json_data(filename:str,number:int):
     try:
         tfile = tarfile.open(filename,"r:xz")
+        logger.info("File No:%f"%number)
         tfile.extractall(path=os.path.join("json",filename))
         tfile.close()
     except:
@@ -32,24 +37,24 @@ def push_data_into_one():
     [shutil.move(os.path.join(path, name), os.path.join(Destination,name)) for path, subdirs, files in os.walk(os.path.join("json","Data")) for name in files if name.endswith('.json')]
     logger.info("Files Movement Completed")
     
-def make_single_file(number:int=150000)->list:
-    files = [os.path.join('json/Final',f) for f in listdir(os.path.join("json","Final"))]
-    limited_files = files[:number]
-    output = []    
-    for item in limited_files: 
-        with open(item,'r') as fp:
-            data = json.load(fp)
-            fp.close() 
-        output.append(data)
-    logger.info("Number of Processed files %s"%len(output))
-    with open("final_copy.json",'w') as fp:
-        json.dump(output,fp)
-        fp.close()
+    
+async def read_file_from_json(filename:str):
+    async with aiofiles.open(filename, mode='r') as f:
+        contents = await f.read()
+        aiofiles.close(f)
+    jsonfile = json.loads(contents)
+    return jsonfile
+    
+async def make_single_file(number:int=300000)->list:
+    files = [asyncio.ensure_future(read_file_from_json(os.path.join('json/Final',f))) for f in listdir(os.path.join("json","Final"))]
+    logger.info("Number Of Files Collected Successfully %s"%len(files))
+    moves_list = await asyncio.gather(*files)
+    async with aiofiles.open('final_copy.json', mode='w') as f:
+        await json.dump(moves_list,f)
     logger.info("File Saved")
     
-
-
+    
 if __name__ == '__main__':
-    extract_json_data("Data")
-    push_data_into_one()
-    make_single_file()
+    #extract_json_data("Data")
+    #push_data_into_one()
+    asyncio.run(make_single_file())
